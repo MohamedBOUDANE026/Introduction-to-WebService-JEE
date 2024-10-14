3d3f87e877296237ade37423964c1b795720fae8
mBoudane <mohamedboudane6@gmail.com>
2024-10-14 19:02:41 +0100

    first commit

---
+++ src/main/java/ServerJWS.java
@@ -0,0 +1,10 @@
+import com.sun.xml.ws.transport.httpspi.servlet.EndpointContextImpl;
+import jakarta.xml.ws.Endpoint;
+import ws.BankService;
+
+public class ServerJWS {
+    public static void main(String[] args) {
+        Endpoint.publish("http://0.0.0.0:9191/", new BankService());
+        System.out.println("Web service deployé sur http://0.0.0.0:9191/");
+    }
+}
